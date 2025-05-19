283aebee5958263dfd55396fba788973e9f2732e
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-05-19 22:20:27 +0600

    post node crud done

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ const port =process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
 
-console.log(process.env.USER_DB,process.env.PASS_DB)
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.off1efx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,6 +21,14 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+    const gardenCollection =client.db("gardenDB").collection("garden")
+    app.post('/garden',async(req,res)=>{
+      const gardenUser =req.body;
+      const result =await gardenCollection.insertOne(gardenUser);
+      res.send(result)
+
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
