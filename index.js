70204dd5f31cff98b53525e35dc4303aeba83e59
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-05-19 22:15:45 +0600

    env file and mongodb setup

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express =require('express')
 const cors =require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app =express()
 const port =process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
+
+console.log(process.env.USER_DB,process.env.PASS_DB)
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.off1efx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+
+  }
+}
+run().catch(console.dir);
 
 app.get('/',(req,res)=>{
     res.send("gardening store getting the server side")
