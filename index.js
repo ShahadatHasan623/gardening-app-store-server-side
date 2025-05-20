03a74047cc5e8f9cf825ace43d6806d34884e5ad
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-05-20 20:30:15 +0600

    added get method

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express =require('express')
-const cors =require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-const app =express()
-const port =process.env.PORT || 3000;
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.off1efx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -15,34 +15,46 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
 
-    const gardenCollection =client.db("gardenDB").collection("garden")
-    app.post('/garden',async(req,res)=>{
-      const gardenUser =req.body;
-      const result =await gardenCollection.insertOne(gardenUser);
-      res.send(result)
+    const gardenCollection = client.db("gardenDB").collection("garden");
 
-    })
+    app.post("/garden", async (req, res) => {
+      const gardenUser = req.body;
+      const result = await gardenCollection.insertOne(gardenUser);
+      res.send(result);
+    });
+
+    app.get("/garden", async (req, res) => {
+      const result = await gardenCollection.find().toArray();
+      res.send(result);
+    });
+    
+    app.get("/garden/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await gardenCollection.findOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    
-
   }
 }
 run().catch(console.dir);
 
-app.get('/',(req,res)=>{
-    res.send("gardening store getting the server side")
-})
+app.get("/", (req, res) => {
+  res.send("gardening store getting the server side");
+});
 
-app.listen(port,()=>{
-    console.log(`server is running at:${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is running at:${port}`);
+});
