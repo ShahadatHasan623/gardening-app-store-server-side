41044a268e80f77c16afd2973ab78b31ce57c494
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-05-22 01:18:16 +0600

    added garden home limit

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ async function run() {
 
     const gardenCollection = client.db("gardenDB").collection("garden");
     const exploreCollection = client.db("gardenersDB").collection("gardeners");
-    const trendingCollection = client.db("trendingDB").collection("trending");
 
     app.post("/garden", async (req, res) => {
       const gardenUser = req.body;
@@ -36,7 +35,12 @@ async function run() {
       const result = await gardenCollection.find().toArray();
       res.send(result);
     });
-    
+
+    app.get("/garden/home", async (req, res) => {
+      const result = await gardenCollection.find().limit(6).toArray();
+      res.send(result);
+    });
+
     app.get("/garden/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -44,48 +48,52 @@ async function run() {
       res.send(result);
     });
 
-    // delete korbo 
-    app.delete('/garden/:id',async(req,res)=>{
-      const id =req.params.id;
-      const filter ={_id: new ObjectId(id)};
-      const result =await gardenCollection.deleteOne(filter)
-      res.send(result)
-      
-    })
+    // delete korbo
+    app.delete("/garden/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await gardenCollection.deleteOne(filter);
+      res.send(result);
+    });
 
-    // update Tip 
-    app.put('/garden/:id',async(req,res)=>{
-      const id =req.params.id;
-      const filter ={_id: new ObjectId(id)};
-      const updateGarden =req.body;
-      const options = { upsert: true }; 
-      const updateDoc={
-        $set:updateGarden
+    // update Tip
+    app.put("/garden/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateGarden = req.body;
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: updateGarden,
       };
-      const result =await gardenCollection.updateOne(filter,updateDoc,options)
-      res.send(result)
-      
-    })
+      const result = await gardenCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
     //  Explore Gardeners:
 
-    app.post('/gardeners',async(req,res)=>{
-      const gardeners =req.body;
-      const result =await exploreCollection.insertOne(gardeners)
-      res.send(result)
-    })
+    app.post("/gardeners", async (req, res) => {
+      const gardeners = req.body;
+      const result = await exploreCollection.insertOne(gardeners);
+      res.send(result);
+    });
 
-    app.get('/gardeners',async(req,res)=>{
-      const result =await exploreCollection.find({status:"active"}).limit(6).toArray()
-      res.send(result)
-    })
+    app.get("/gardeners", async (req, res) => {
+      const result = await exploreCollection
+        .find({ status: "active" })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
 
     // top trending data
-    app.post('/trending',async(req,res)=>{
-      const trending =req.body;
-      const result =await trendingCollection.insertOne(trending);
-      res.send(result)
-    })
-
+    app.post("/trending", async (req, res) => {
+      const trending = req.body;
+      const result = await trendingCollection.insertOne(trending);
+      res.send(result);
+    });
 
     // await client.db("admin").command({ ping: 1 });
     // console.log(
